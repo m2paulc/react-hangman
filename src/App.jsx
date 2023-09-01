36f858e250259f948a7aeb7a8a4320cdfea9f477
Paul Caoile <hyp3rsp8c3@gmail.com>
2023-09-01 10:48:05 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Hangman from './components/Hangman';
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Hangman />
+      </div>
+    </>
+  );
+}
+
+export default App;
